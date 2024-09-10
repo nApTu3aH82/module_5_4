ba4e3884c2fe30261113e8dd5660dee74dc31836
nApTu3aH82 <rinsy89@gmail.com>
2024-09-10 14:24:42 +0500

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,73 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, floor):
+        self.floor = floor
+        if floor > self.number_of_floors:
+            print('Такого этажа не существует!')
+        else:
+            for count in range(floor):
+                print(count + 1)
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+    def __len__(self):
+        return (self.number_of_floors)
+
+    def __str__(self):
+        return (f'Название {self.name}, количество этажей: {self.number_of_floors}')
+
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрешки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
